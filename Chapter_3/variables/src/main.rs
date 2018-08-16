a76db12a01af7fc44e8d644ea728bf6327ba11cf
Christos Nianiakas <cnianiak@gmail.com>
2018-08-16 22:42:52 +0300

    Added Chapter_3/variables example.

---
+++ Chapter_3/variables/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    const MAX_POINTS: u32 = 100_000;
+    println!("The value of MAX_POINTS is: {}", MAX_POINTS);
+
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("The value of spaces is: {}", spaces);
+}
