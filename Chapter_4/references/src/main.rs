0ed5718486803db6437e4ca563dbc76dcba1d238
Christos Nianiakas <cnianiak@gmail.com>
2021-09-21 18:54:35 +0300

    Added Chapter_4/references example

---
+++ Chapter_4/references/src/main.rs
@@ -0,0 +1,40 @@
+fn main() {
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+
+    let mut s = String::from("hello");
+
+    change(&mut s);
+
+    println!("{}", s);
+
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
+
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
+
+    let no_dangle_s = no_dangle();
+    println!("{}", no_dangle_s);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+fn no_dangle() -> String {
+    let s = String::from("hello");
+
+    s
+}
